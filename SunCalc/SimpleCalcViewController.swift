9058f091815a9e12b1f3d544fbf6aba286af6d19
LingDar <KanePharm@hotmail.com>
2017-10-18 09:42:49 -0500

    First Commit - Have created Empty ViewController for simple calc page NEXT: Connect ot GitHub

---
+++ SunCalc/SimpleCalcViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SimpleCalcViewController.swift
+//  SunCalc
+//
+//  Created by Brian Kane on 10/18/17.
+//  Copyright © 2017 BDKCreate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SimpleCalcViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
